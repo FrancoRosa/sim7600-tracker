e3e7f87af67402c4366f0b83282c088edcb233d4
FrancoRosa <km115.franco@gmail.com>
2021-06-17 11:23:14 +1000

    Get imei

--- sim7600_tracker.ino
+++ sim7600_tracker.ino
@@ -8,8 +8,9 @@
 #include <FreeRTOS_SAMD21.h>
 
 #define led_pin 13
-#define Modem Serial1
-#define SerialUSB Serial
+#define key_pin 2
+// #define Modem Serial1
+// #define SerialUSB Serial
 
 const char domain[] = "iotnetwork.com.au:5055";
 
@@ -20,7 +21,7 @@ volatile bool flagGNS = false;
 volatile bool flagHTTPACT = false;
 volatile bool flagDOWNLOAD = false;
 
-char GSN[15];
+char GSN[]="000000000000000";
 
 const int modem_size = 120;       // Size of Modem serila buffer
 char modem_buffer[modem_size];    // Modem serial buffer 
@@ -45,58 +46,66 @@ const char mRIN[] = "RING"; // when someones makes a call.. it should hang up
 const char mHUP[] = "ATH";  // when someones makes a call.. it should hang up
 // - - - - - - - - - - - - - - - - - - - - - - - 
 
-volatile bool flagPromt = false;
-
-
 TaskHandle_t Handle_rxTask;
 TaskHandle_t Handle_txTask;
 
-void osDelayUs(int us)
-{
+// - - - - - - - - Delay Helpers - - - - - - - - 
+void osDelayUs(int us) {
   vTaskDelay( us / portTICK_PERIOD_US );  
 }
 
-void osDelayMs(int ms)
-{
+void osDelayMs(int ms) {
   vTaskDelay( (ms * 1000) / portTICK_PERIOD_US );  
 }
 
-void osDelayS(int s)
-{
+void osDelayS(int s) {
   while (s>0){
     osDelayMs(1000);
     s--;    
   }  
 }
+// - - - - - - - - - - - - - - - - - - - - - - - 
+// - - - - - - - - Print helpers- - - - - - - - - 
+void printBuffer(Stream &serial, char *buffer) {
+  char c;
+  int i = 0;
+  int len = strlen(buffer);
+  while (true) {
+    c = buffer[i];
+    i++;
+    if (c == 0 || i > len) break;
+    serial.print(c);
+  }
+}
+// - - - - - - - - - - - - - - - - - - - - - - - 
 
-void mPower() {
-  SerialUSB.println("... restarting modem");
+
+void modemPower() {
+  Serial.println("... restarting modem");
+  digitalWrite(key_pin, LOW);
+  osDelayS(1);
+  digitalWrite(key_pin, HIGH);
 }
 
 // Read modem IMEI
-void procGSN() 
-{
-  int n=0;
-  int k=0;
-  modem_buffer[15]=0;
-  for(n=0;n<13;n=n+2)
-  {
-    GSN[k]=(modem_buffer[n]-48)*10+(modem_buffer[n+1]-48);
-    k++;
-  }
-  GSN[7]=modem_buffer[14]-48;
+void procGSN() {
+  Serial.println("... procesing GSN");
+  memcpy(GSN, modem_buffer, 15);
+  Serial.print("GSN: ");
+  Serial.print(GSN);
+  printBuffer(Serial, GSN);
 }
 
 bool sendCommand(const char *command,int timeout) {
-  Modem.print("AT+");
-  Modem.print(command);
-  Modem.print("\r");
+  Serial1.print("AT+");
+  Serial1.print(command);
+  Serial1.print("\r");
   return waitOk(timeout);
 }
 
 bool waitOk(int timeout) {
-  flagOK=0;
-  flagERROR=0;
+  flagOK = false;
+  flagERROR= false;
   timeout= timeout*10;
   int t = 0;
   while (timeout > t)   {
@@ -107,7 +116,7 @@ bool waitOk(int timeout) {
     }
     osDelayMs(100);
   }
-  mPower();
+  modemPower();
   return false;
 }
 
@@ -116,30 +125,30 @@ bool waitOk(int timeout) {
 
 static void task_rx_modem(void *pvParameters) {
   while (true){
-    while(Modem.available()) {
-      modem_char = Modem.read();
-      SerialUSB.print(modem_char);
+    while(Serial1.available()) {
+      modem_char = Serial1.read();
+      Serial.print(modem_char);
       modem_buffer[modem_i] = modem_char;
       modem_i++;
       if (modem_i >= modem_size) modem_i = 0;
-      if (modem_char=='>') flagPromt = true;
-      if ((modem_i>=2) && ((modem_char == '\n')|| (modem_char == '\n')))
+      // if (modem_char=='>') flagPromt = true;
+      if ((modem_i >= 2) && ((modem_char == '\n') || (modem_char == '\n')))
         {
           modem_buffer[modem_i]='\0';
           if (memcmp(mOK,    modem_buffer,2)==0) flagOK=true;
-          if (memcmp(mERROR, modem_buffer,4)==0) flagERROR=true;
-          if (memcmp(mCLOSED,modem_buffer,4)==0) flagConn=false;
-          if (memcmp(mSend,  modem_buffer,6)==0) flagSend=true;
-          if (memcmp(mConn,  modem_buffer,9)==0) flagConn=true;
-          if (memcmp(mRIN,   modem_buffer,4)==0) flagRIN=true;
-          if (memcmp(mCBC,   modem_buffer,4)==0) procCBC();
-          if (memcmp(mCSQ,   modem_buffer,4)==0) procCSQ();
-          if (memcmp(mCGR,   modem_buffer,4)==0) procCGR();
-          if (memcmp(mCGN,   modem_buffer,4)==0) procCGN();
+          // if (memcmp(mERROR, modem_buffer,4)==0) flagERROR=true;
+          // if (memcmp(mCLOSED,modem_buffer,4)==0) flagConn=false;
+          // if (memcmp(mSend,  modem_buffer,6)==0) flagSend=true;
+          // if (memcmp(mConn,  modem_buffer,9)==0) flagConn=true;
+          // if (memcmp(mRIN,   modem_buffer,4)==0) flagRIN=true;
+          // if (memcmp(mCBC,   modem_buffer,4)==0) procCBC();
+          // if (memcmp(mCSQ,   modem_buffer,4)==0) procCSQ();
+          // if (memcmp(mCGR,   modem_buffer,4)==0) procCGR();
+          // if (memcmp(mCGN,   modem_buffer,4)==0) procCGN();
           if (memcmp(mGSN,   modem_buffer,2)==0) procGSN();
-          if (memcmp(mCOP,   modem_buffer,4)==0) procCOP();
+          // if (memcmp(mCOP,   modem_buffer,4)==0) procCOP();
           modem_i=0;
-          for(i=0;i<modem_size;i++) modem_buffer[i]=0; 
+          for(int i = 0; i < modem_size; i++) modem_buffer[i]=0; 
         }
     }
    osDelayMs(1);
@@ -147,9 +156,10 @@ static void task_rx_modem(void *pvParameters) {
 }
 
 static void task_tx_modem(void *pvParameters) {
+  modemPower(); 
   while (true) {
     osDelayS(10);
-    SerialUSB.println(".... uart test");
+    Serial.println(".... uart test");
     sendCommand("GSN", 5);
     digitalWrite(led_pin, !digitalRead(led_pin));
   }
@@ -157,16 +167,19 @@ static void task_tx_modem(void *pvParameters) {
 
 void setup() {
   pinMode(led_pin, OUTPUT);
-  SerialUSB.begin(115200);
+  pinMode(key_pin, OUTPUT);
+  digitalWrite(led_pin, HIGH);
+  digitalWrite(key_pin, HIGH);
+  Serial.begin(115200);
   delay(1000); // keep this to avoid USB crash
-  Modem.begin(115200);
+  Serial1.begin(115200);
   
   xTaskCreate(task_tx_modem, "txModem", 256, NULL, tskIDLE_PRIORITY + 2, &Handle_txTask);
   xTaskCreate(task_rx_modem, "rxModem", 256, NULL, tskIDLE_PRIORITY + 1, &Handle_rxTask);
   vTaskStartScheduler();
 
   while(true) {
-    SerialUSB.println("OS Failed! \n");
+    Serial.println("OS Failed! \n");
     delay(1000);
   }
 }
